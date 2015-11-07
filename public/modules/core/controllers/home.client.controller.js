d0e48aa23e0af5f9eccafe110455731838ee70c5
Syed Ahmed Anas <ahmed.a.tamim@gmail.com>
2015-11-07 17:46:32 +0500

    more stuff

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -10,40 +10,40 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			{
 				icon: 'glyphicon-user',
 				colour: 'btn-success',
-				total: "20,400",
-				description: "TOTAL CUSTOMERS"
+				total: '20,400',
+				description: 'TOTAL CUSTOMERS'
 			},
 			{
 				icon: 'glyphicon-calendar',
 				colour: 'btn-primary',
-				total: "8,382",
-				description: "UPCOMING EVENTS"
+				total: '8,382',
+				description: 'UPCOMING EVENTS'
 			},
 			{
 				icon: 'glyphicon-edit',
 				colour: 'btn-success',
-				total: "527",
-				description: "NEW CUSTOMERS IN 24H"
+				total: '527',
+				description: 'NEW CUSTOMERS IN 24H'
 			},
 			{
 				icon: 'glyphicon-record',
 				colour: 'btn-info',
-				total: "85,000",
-				description: "EMAILS SENT"
+				total: '85,000',
+				description: 'EMAILS SENT'
 			},
 			{
 				icon: 'glyphicon-eye-open',
 				colour: 'btn-warning',
-				total: "268",
-				description: "FOLLOW UP REQUIRED"
+				total: '268',
+				description: 'FOLLOW UP REQUIRED'
 			},
 			{
 				icon: 'glyphicon-flag',
 				colour: 'btn-danger',
-				total: "348",
-				description: "REFERRALS TO MODERATE"
+				total: '348',
+				description: 'REFERRALS TO MODERATE'
 			}
 
-		]
+		];
 	}
 ]);
